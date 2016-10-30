39fa57cbbe0ce320d24d0e6f7aea867fa6106d12
VinstonWolf <konstantin24121@gmail.com>
2016-10-30 22:50:57 +0500

    start thinking about file structure

---
+++ src/client.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const dest = document.getElementById('app');
+ReactDOM.render(
+  <div>
+  	App
+  </div>,
+  dest
+);
+
--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log('This this a app,  Bitch');
